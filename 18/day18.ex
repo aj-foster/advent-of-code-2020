43ae3814ee25f63a3b0c903a19bc3e4026861b52
AJ Foster <web@aj-foster.com>
2020-12-18 01:13:11 -0500

    Day 18 minor cleanup

--- 18/day18.ex
+++ 18/day18.ex
@@ -1,26 +1,40 @@
 defmodule Day18 do
-  @spec part_one :: number
-  def part_one do
+  #
+  # Read and Parse
+  #
+
+  defp input do
     File.stream!("18/input.txt")
     |> Stream.map(&String.trim/1)
     |> Stream.map(&parse/1)
-    |> Stream.map(&evaluate/1)
-    |> Enum.sum()
   end
 
   defp parse(input, result \\ [])
+
   defp parse(<<" ", rest::binary>>, result), do: parse(rest, result)
   defp parse(<<"(", rest::binary>>, result), do: parse(rest, [:< | result])
   defp parse(<<")", rest::binary>>, result), do: parse(rest, [:> | result])
   defp parse(<<"+", rest::binary>>, result), do: parse(rest, [:+ | result])
   defp parse(<<"*", rest::binary>>, result), do: parse(rest, [:* | result])
+  defp parse("", result), do: Enum.reverse(result)
 
   defp parse(<<number::binary-1, rest::binary>>, result),
     do: parse(rest, [String.to_integer(number) | result])
 
-  defp parse("", result), do: Enum.reverse(result)
+  #
+  # Part One
+  #
+
+  @spec part_one :: number
+  def part_one do
+    input()
+    |> Stream.map(&evaluate/1)
+    |> Enum.sum()
+  end
 
   defp evaluate(input, result \\ 0, operation \\ nil)
+  defp evaluate([], result, nil), do: result
+
   defp evaluate([:+ | rest], result, _op), do: evaluate(rest, result, &Kernel.+/2)
   defp evaluate([:* | rest], result, _op), do: evaluate(rest, result, &Kernel.*/2)
   defp evaluate([num | rest], 0, nil) when is_number(num), do: evaluate(rest, num, nil)
@@ -39,30 +53,26 @@ defmodule Day18 do
       end)
       |> Enum.reverse()
 
-    result =
-      if operation do
-        evaluate(subset, 0, nil)
-        |> operation.(result)
-      else
-        evaluate(subset, 0, nil)
-      end
+    value = evaluate(subset, 0, nil)
+    rest = Enum.drop(rest, length(subset) + 1)
 
-    Enum.drop(rest, length(subset) + 1)
-    |> evaluate(result, nil)
+    evaluate([value | rest], result, operation)
   end
 
-  defp evaluate([], result, nil), do: result
+  #
+  # Part Two
+  #
 
   @spec part_two :: number
   def part_two do
-    File.stream!("18/input.txt")
-    |> Stream.map(&String.trim/1)
-    |> Stream.map(&parse/1)
+    input()
     |> Stream.map(&evaluate2/1)
     |> Enum.sum()
   end
 
   defp evaluate2(input, result \\ 0, operation \\ nil)
+  defp evaluate2([], result, nil), do: result
+
   defp evaluate2([:+ | rest], result, _op), do: evaluate2(rest, result, :+)
   defp evaluate2([:* | rest], result, _op), do: evaluate2(rest, result, :*)
   defp evaluate2([num | rest], 0, nil) when is_number(num), do: evaluate2(rest, num, nil)
@@ -90,6 +100,4 @@ defmodule Day18 do
 
     evaluate2([value | rest], result, operation)
   end
-
-  defp evaluate2([], result, nil), do: result
 end
