42a6e3a670ecdf386171933c14e8b7b8f2b67d71
AJ Foster <web@aj-foster.com>
2020-12-19 23:55:01 -0500

    Day 19 minor cleanup

--- 19/day19.ex
+++ 19/day19.ex
@@ -1,19 +1,22 @@
 defmodule Day19 do
+  #
+  # Read and Parse
+  #
+
   defp input do
     File.stream!("19/input.txt")
     |> Stream.map(&String.trim/1)
     |> Stream.map(&parse_input/1)
   end
 
+  @rule ~r/(?<rule>\d+)\: (((?<r11>\d+)( (?<r12>\d+))?( (?<r13>\d+))?( \| (?<r21>\d+)( (?<r22>\d+))?)?)|("(?<letter>\w)"))/
+
   defp parse_input(""), do: nil
   defp parse_input("a" <> rest), do: {:msg, "a" <> rest}
   defp parse_input("b" <> rest), do: {:msg, "b" <> rest}
 
   defp parse_input(rule) do
-    Regex.named_captures(
-      ~r/(?<rule>\d+)\: (((?<r11>\d+)( (?<r12>\d+))?( (?<r13>\d+))?( \| (?<r21>\d+)( (?<r22>\d+))?)?)|("(?<letter>\w)"))/,
-      rule
-    )
+    Regex.named_captures(@rule, rule)
     |> Map.update!("rule", &String.to_integer/1)
     |> Enum.reject(fn {_key, value} -> value == "" end)
     |> Enum.into(%{})
@@ -33,6 +36,7 @@ defmodule Day19 do
         r21 = String.to_integer(r21)
         {:rule, rule, {[r11], [r21]}}
 
+      # Used in the demo.
       %{"r11" => r11, "r12" => r12, "r13" => r13, "rule" => rule} ->
         r11 = String.to_integer(r11)
         r12 = String.to_integer(r12)
@@ -54,6 +58,7 @@ defmodule Day19 do
   # Part One
   #
 
+  @spec part_one :: non_neg_integer
   def part_one do
     stream = input()
 
@@ -67,9 +72,7 @@ defmodule Day19 do
     stream
     |> Stream.filter(&match?({:msg, _}, &1))
     |> Stream.map(&elem(&1, 1))
-    # |> Stream.filter(&(String.length(&1) in List.flatten(valid?(&1, rules))))
     |> Stream.filter(&valid?(&1, rules))
-    # |> Stream.filter(fn line -> Regex.match?(rules, line) end)
     |> Enum.count()
   end
 
@@ -94,79 +97,7 @@ defmodule Day19 do
     {replace_rule(rule1, target, replacement), replace_rule(rule2, target, replacement)}
   end
 
-  # defp process_rules([{:rule, 0, rule}]) do
-  #   string = List.flatten(rule) |> Enum.join()
-  #   Regex.compile!("^" <> string <> "$")
-  # end
-
-  # defp process_rules([{:rule, num, rule} | rest]) do
-  #   rest
-  #   |> Enum.map(fn {:rule, n, r} -> {:rule, n, replace_rule(r, num, rule)} end)
-  #   |> process_rules()
-  # end
-
-  # defp replace_rule("a", _, _), do: "a"
-  # defp replace_rule("b", _, _), do: "b"
-  # defp replace_rule(target, target, replacement), do: replacement
-  # defp replace_rule(num, _, _) when is_number(num), do: num
-  # defp replace_rule(char, _, _) when is_binary(char), do: char
-
-  # defp replace_rule(rule, target, replacement) when is_list(rule) do
-  #   Enum.map(rule, &replace_rule(&1, target, replacement))
-  # end
-
-  # defp replace_rule({rule1, rule2}, target, replacement) do
-  #   [
-  #     "(",
-  #     replace_rule(rule1, target, replacement),
-  #     "|",
-  #     replace_rule(rule2, target, replacement),
-  #     ")"
-  #   ]
-  # end
-
-  # defp valid?(message, rules)
-
-  # defp valid?(msg, []), do: byte_size(msg) == 0
-  # defp valid?("a" <> _rest, "a"), do: true
-  # defp valid?("a" <> _rest, "b"), do: false
-  # defp valid?("b" <> _rest, "b"), do: true
-  # defp valid?("b" <> _rest, "a"), do: false
-  # defp valid?(msg, {rule1, rule2}), do: valid?(msg, rule1) or valid?(msg, rule2)
-
-  # defp valid?(msg, [rule | rest]) do
-  #   valid?(msg, rule) and valid?(m, rest)
-  # end
-
-  # defp valid?(message, rules, count \\ 0)
-
-  # defp valid?("", _, count), do: count
-  # defp valid?(_msg, [], count), do: count
-  # defp valid?("a" <> _rest, "a", counts) when is_list(counts), do: Enum.map(counts, &(&1 + 1))
-  # defp valid?("b" <> _rest, "b", counts) when is_list(counts), do: Enum.map(counts, &(&1 + 1))
-  # defp valid?("a" <> _rest, "a", count), do: count + 1
-  # defp valid?("b" <> _rest, "b", count), do: count + 1
-  # defp valid?("a" <> _rest, "b", count), do: count
-  # defp valid?("b" <> _rest, "a", count), do: count
-
-  # defp valid?(msg, {r1, r2}, c), do: List.flatten([valid?(msg, r1, c), valid?(msg, r2, c)])
-
-  # defp valid?(msg, [rule | rest], count) do
-  #   case valid?(msg, rule, count) do
-  #     counts when is_list(counts) ->
-  #       counts
-  #       |> List.flatten()
-  #       |> Enum.map(fn count ->
-  #         valid?(String.slice(msg, count..-1), rest, count)
-  #       end)
-
-  #     count ->
-  #       msg = String.slice(msg, count..-1)
-  #       valid?(msg, rest, count)
-  #   end
-  # end
-
-  #  defp valid?(message, rules)
+  defp valid?(message, rules)
 
   defp valid?("a" <> rest, ["a" | rules]), do: valid?(rest, rules)
   defp valid?("b" <> rest, ["b" | rules]), do: valid?(rest, rules)
@@ -185,26 +116,11 @@ defmodule Day19 do
   defp valid?("", _rules), do: false
   defp valid?(_msg, []), do: false
 
-  # defp valid?(message, rules)
-
-  # defp valid?(_msg, []), do: 0
-  # defp valid?("a" <> _rest, "a"), do: 1
-  # defp valid?("b" <> _rest, "b"), do: 1
-  # defp valid?("a" <> _rest, "b"), do: 0
-  # defp valid?("b" <> _rest, "a"), do: 0
-
-  # defp valid?(msg, {r1, r2}), do: max(valid?(msg, r1), valid?(msg, r2))
-
-  # defp valid?(msg, [rule | rest]) do
-  #   count = valid?(msg, rule)
-  #   msg = String.slice(msg, count..-1)
-  #   valid?(msg, rest)
-  # end
-
   #
   # Part Two
   #
 
+  @spec part_two :: non_neg_integer
   def part_two do
     stream = input()
 
@@ -219,9 +135,7 @@ defmodule Day19 do
     stream
     |> Stream.filter(&match?({:msg, _}, &1))
     |> Stream.map(&elem(&1, 1))
-    # |> Stream.filter(&(String.length(&1) in List.flatten(valid?(&1, rules))))
     |> Stream.filter(&valid?(&1, rules))
-    # |> Stream.filter(fn line -> Regex.match?(rules, line) end)
     |> Enum.count()
   end
 
