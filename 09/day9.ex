19b601cd708ca7e1b73b299336e898d2257fc799
AJ Foster <web@aj-foster.com>
2020-12-21 01:48:34 -0500

    Day 9 minor cleanup

--- 09/day9.ex
+++ 09/day9.ex
@@ -1,12 +1,24 @@
 defmodule Day9 do
-  def part_one do
+  #
+  # Read and Parse
+  #
+
+  defp input do
     File.stream!("09/input.txt")
     |> Stream.map(&String.trim/1)
     |> Stream.map(&String.to_integer/1)
-    |> Enum.reduce_while({[], []}, &stuff/2)
   end
 
-  defp stuff(num, {last_25, cross}) do
+  #
+  # Part One
+  #
+
+  @spec part_one :: integer
+  def part_one do
+    Enum.reduce_while(input(), {[], []}, &search_for_sum/2)
+  end
+
+  defp search_for_sum(num, {last_25, cross}) do
     if length(last_25) == 25 do
       case num in List.flatten(cross) do
         true ->
@@ -34,26 +46,24 @@ defmodule Day9 do
     end
   end
 
+  #
+  # Part Two
+  #
+
+  @target 258_585_477
+
+  @spec part_two :: number
   def part_two do
-    File.stream!("09/input.txt")
-    |> Stream.map(&String.trim/1)
-    |> Stream.map(&String.to_integer/1)
-    |> Enum.reduce_while([], &stuff2/2)
+    Enum.reduce_while(input(), [], &stuff2/2)
   end
 
   defp stuff2(num, range) do
-    sum =
-      if length(range) > 0 do
-        Enum.reduce(range, &+/2)
-      else
-        0
-      end
+    sum = Enum.reduce(range, 0, &+/2)
 
-    if sum == 258_585_477 do
-      IO.inspect(range)
+    if sum == @target do
       {:halt, Enum.min(range) + Enum.max(range)}
     else
-      if sum > 258_585_477 do
+      if sum > @target do
         {_, range} = List.pop_at(range, -1)
         stuff2(num, range)
       else
